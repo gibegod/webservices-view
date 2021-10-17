3f23027b4f91dab18add1b0761da427bda940363
Joel Stella <stella.joelivan@gmail.com>
2021-10-17 02:31:24 -0300

    Actualización estados

--- correo-view/src/pages/Home.js
+++ correo-view/src/pages/Home.js
@@ -3,11 +3,11 @@ import Cookies from "universal-cookie";
 import GenericNav from "../components/GenericNav";
 import { Table } from "react-bootstrap";
 import axios from "axios";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import "../css/Home.css";
 
 const cookies = new Cookies();
 const urlGetEnvios = "http://localhost:9001/api/v1.0/envios";
+const urlPatchEnvio = "http://localhost:9001/api/v1.0/envio";
 
 class Home extends Component{
     state={
@@ -48,6 +48,21 @@ class Home extends Component{
         this.setState({envios: search})
     }
 
+    actualizarEstados=()=>{
+        var envios = document.getElementsByClassName("envio");
+        for(var i=0; i<envios.length; i++){
+            var cod_seguimiento = envios[i].childNodes[0].innerHTML;
+            var estado = envios[i].childNodes[3].childNodes[0].value;
+            
+            this.peticionPatchEnvio(cod_seguimiento, estado);
+        }  
+        window.location.href="./Home";      
+    }
+
+    peticionPatchEnvio=(cod_seguimiento, estado)=>{
+        axios.patch(urlPatchEnvio, {params:{cod_seguimiento:cod_seguimiento, estado:estado}})
+    }
+
     render(){
         return (
             <div>
@@ -69,24 +84,31 @@ class Home extends Component{
                                 <th>Código seguimiento</th>
                                 <th>DNI destinatario</th>
                                 <th>Estado</th>
-                                <th>Acciones</th>
+                                <th>Nuevo estado</th>
                             </tr>
                         </thead>
                         <tbody>
                             {this.state.envios.map(envio=>{
                                 return (
-                                    <tr>
+                                    <tr className="envio">
                                         <td>{envio.cod_seguimiento}</td>
                                         <td>{envio.dni_destinatario}</td>
                                         <td>{envio.estado}</td>
                                         <td>
-                                            <button className="btn btn-primary"><FontAwesomeIcon icon={faEdit}/></button>
+                                            <select className="form-select">
+                                                <option disabled selected value={envio.estado}>Seleccione...</option>
+                                                <option value="En preparación">En preparación</option>
+                                                <option value="Despachado">Despachado</option>
+                                                <option value="En Camino">En Camino</option>
+                                                <option value="Entregado">Entregado</option>
+                                            </select>
                                         </td>
                                     </tr>
                                 )
                             })}                            
                         </tbody>
                     </Table>
+                    <button className="btn btn-success btn-actualizar-estados" onClick={this.actualizarEstados}>Actualizar estados</button>
                 </div>
             </div>            
         );
